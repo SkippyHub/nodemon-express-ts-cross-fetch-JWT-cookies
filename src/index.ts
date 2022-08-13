19a683c76781793a93b3048665dbe51499975ffb
SkippyHub <79400098+SkippyHub@users.noreply.github.com>
2022-08-13 09:08:33 +0100

    +docker fetch

--- src/index.ts
+++ src/index.ts
@@ -2,8 +2,20 @@
 import express from 'express';
 import cors from 'cors';
 // import fetch from 'node-fetch';
-import fetch from 'cross-fetch';
+import fetch, { Request, Response, Headers } from 'cross-fetch';
+
+// import cookie parse
+// import cookie 
+import cookie from 'cookie';
+import cookieParser from 'cookie-parser';
+
+
+// import jasonwebtoken
+import jwt from 'jsonwebtoken';
+
+
 import dotenv from 'dotenv';
+import { json } from 'stream/consumers';
 dotenv.config();
 const USERNAME = process.env.USERNAME
 const PASSWORD = process.env.PASSWORD
@@ -27,37 +39,60 @@ app.get('/', async (req, res) => {
     res.send('Hello World!');
 });
 
+
+// here is an example of fetching docker repos with cross-fetch, cookie parser and jwt
+//https://github.com/lquixada/cross-fetch/blob/main/examples/typescript/index.ts
 // lamda async fetch response
 const loginLamda = async (username: string, password: string) => {
-    
+
     let url = 'https://hub.docker.com//v2/users/login';
 
     let options = {
-    method: 'POST',
-    headers: {
-        'Content-Type': 'application/json',
-    },
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            Accept: 'application/json'
+        },
 
-    body: `{"username":"${username}","password":"${password}"}`
+        body: `{"username":"${username}","password":"${password}"}`
     };
-// docker async fetch response
+    // docker async fetch response
     const docker = await fetch(url, options)
         .then(res => res)
-        // .then(json => {
-        //     console.log(json);
-        //     return json;
-        // })
-        // .catch(err => {
-        //     console.log(err);
-        // });
-        console.log(docker.headers.get('set-cookie'));
-        console.log(await docker.json())
-    
-        // const response = await fetch('https://example.com');
 
-        // // Returns an array of values, instead of a string of comma-separated values
-        // console.log(response.headers.raw()['set-cookie']);
 
+    const body = await docker.json()
+
+    // this is not the normal way to do this as this is a hack due to dockerhub api being broken.
+    const cookies = cookie.parse(docker.headers.get('set-cookie') ?? "".split(',')[0]);
+
+
+    let token = body.token;
+    console.log("token: ", token);
+    let csrftoken = cookieParser.JSONCookies(cookies).csrftoken;
+    console.log("csrftoken: ", csrftoken);
+
+    // decode the token to get the user id  
+    // type issues need to assign decoded payload variable as any type 
+    let decoded: any = jwt.decode(token);
+
+    // let datajson = JSON.stringify(decoded);
+    // console.log(datajson);
+
+    let namespaces = decoded?.username ?? "";
+    let repourl = `https://hub.docker.com//v2/namespaces/${namespaces}/repositories/`;
+
+    let repooptions = {
+        method: 'GET',
+        headers: {
+            Authorization: `Bearer ${token}`,
+            cookie: `csrftoken=${csrftoken};token=${token}`
+        }
+    };
+    const dockerrepo = await fetch(repourl, repooptions)
+    console.log("dockerrepo: ", dockerrepo);
+    const repobody = await dockerrepo.json()
+    console.log("repobody: ", repobody);
 }
 // console log loginLamda();
 loginLamda(`${USERNAME}`, `${PASSWORD}`);
